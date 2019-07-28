65a3d26e0f68e51161d4106dd81528b0634a80db
Parmiss1360 <51405971+Parmiss1360@users.noreply.github.com>
2019-07-29 00:38:12 +0200

    Add project files.

---
+++ RazorPages/Pages/Contact.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPages.Pages
+{
+    public class ContactModel : PageModel
+    {
+        public int? id { get; set; }
+        public string username { get; set; }
+        public void OnGet(int? id,string username)
+        {
+            this.id = id;
+            this.username = username;
+            //return RedirectToPage("/Store/Index");
+            //return RedirectToPage("/Publisher/Index", new { area = "Admin" });
+        }
+    }
+}
